6895956feb07a2523959f43e4cef2ac6134b4f89
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-11-01 04:01:54 -0300

    add lombok

--- src/main/java/com/stor/car/uploads/FileUpload.java
+++ src/main/java/com/stor/car/uploads/FileUpload.java
@@ -1,30 +1,11 @@
 package com.stor.car.uploads;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class FileUpload {
     private String mimeType;
     private String base64;
-
-    public String getMimeType() {
-        return mimeType;
-    }
-
-    public void setMimeType(String mimeType) {
-        this.mimeType = mimeType;
-    }
-
-    public String getBase64() {
-        return base64;
-    }
-
-    public void setBase64(String base64) {
-        this.base64 = base64;
-    }
-
-    @Override
-    public String toString() {
-        return "FileUpload{" +
-                ", mimeType='" + mimeType + '\'' +
-                ", base64='" + base64 + '\'' +
-                '}';
-    }
 }
